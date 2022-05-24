68fe60db420df05f449446480ba8d213db7b59cf
Debbie <asamoahjaeldeborah@gmail.com>
2022-05-24 11:21:24 +0000

    Created the apiboxes for hive

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,37 @@ import 'package:tab/screens/splash_screen.dart';
 // ignore: constant_identifier_names
 const String ApiBox = "apiData";
 const String ApiBox2 = "apiData2";
+ const String ApiBox3 = "apiData3";
+ const String ApiBox4 = "apiData4";
+ const String ApiBox5 = "apiData5";
+ const String ApiBox6 = "apiData6";
+ const String ApiBox7 = "apiData7";
+ const String ApiBox8 = "apiData8";
+ const String ApiBox9 = "apiData9";
+ const String ApiBox10 = "apiData10";
+ const String ApiBox11 = "apiData11";
+ const String ApiBox12 = "apiData12";
+ const String ApiBox13 = "apiData13";
+ const String ApiBox14 = "apiData14";
+ const String ApiBox15 = "apiData15";
+ const String ApiBox16 = "apiData16";
 
 void main() async {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   await Hive.initFlutter();
   await Hive.openBox(ApiBox);
   await Hive.openBox(ApiBox2);
+  await Hive.openBox(ApiBox3);
+  await Hive.openBox(ApiBox4);
+  await Hive.openBox(ApiBox5);
+  await Hive.openBox(ApiBox6);
+  await Hive.openBox(ApiBox7);
+  await Hive.openBox(ApiBox8);
+  await Hive.openBox(ApiBox9);
+  await Hive.openBox(ApiBox10);
+  await Hive.openBox(ApiBox11);
+  await Hive.openBox(ApiBox12);
+  await Hive.openBox(ApiBox13);
   await Hive.box(ApiBox).clear();
   await Hive.box(ApiBox2).clear();
   ErrorWidget.builder = (FlutterErrorDetails) =>
